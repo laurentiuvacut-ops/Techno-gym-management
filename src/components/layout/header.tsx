423c4a12bb7b75418bfc6ce10e653d24812d5340
Laurentiu Vacut <laurentiuvacut@gmail.com>
2026-01-31 00:25:48 +0000

    Try fixing this error: `Recoverable Error: Hydration failed because the

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { User } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 import { useUser } from '@/firebase';
@@ -19,7 +18,7 @@ export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 h-16 bg-background/80 backdrop-blur-sm z-50 flex items-center border-b">
       <div className="container mx-auto px-4 w-full flex justify-between items-center">
-        <Link href={isClient && user ? "/dashboard" : "/"} className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
            <Image src="https://i.imgur.com/QdArxUJ.png" alt="Techno Gym Logo" width={32} height={32} className="object-contain" />
            <span className="text-xl font-bold tracking-tight"><span className="text-primary">TECHNO</span><span className="text-foreground">GYM</span></span>
         </Link>
