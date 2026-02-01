e59b2a5b7e930f3fd8fa20382182d5923c203ef5
Laurentiu Vacut <laurentiuvacut@gmail.com>
2026-02-01 22:55:22 +0000

    Nu cred ca este de acolo, mi-a mai dat eroarea asta si inainte sa faci a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/layout/header';
 import BottomNav from '@/components/layout/bottom-nav';
 import { FirebaseClientProvider } from '@/firebase';
 import MainContainer from '@/components/layout/main-container';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Techno Gym',
@@ -25,7 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <Script src="https://www.google.com/recaptcha/enterprise.js?render=6LcqwlosAAAAAPx4W8ob5pleV4w6qbHERxyhcWYC" strategy="beforeInteractive" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
