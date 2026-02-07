9a9b7618d8a61d7456ff4a38ffc15edfd6ece909
Laurentiu Vacut <laurentiuvacut@gmail.com>
2026-02-07 11:05:57 +0000

    Inainte: https://imgur.com/0a5HHpP      DUPA: https://imgur.com/vqvTyuV

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -198,4 +198,11 @@ export const transformations = [
     before: getImage('transformation-before-9'),
     after: getImage('transformation-after-9'),
   },
+  {
+    id: 10,
+    name: 'Membru Nou',
+    story: 'O nouă transformare de succes!',
+    before: getImage('transformation-before-10'),
+    after: getImage('transformation-after-10'),
+  },
 ];
