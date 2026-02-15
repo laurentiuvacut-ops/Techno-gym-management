e0ff3cbf0ca5f3ef7cf1b64092cc8b80bb6bd136
Laurentiu Vacut <laurentiuvacut@gmail.com>
2026-02-15 20:39:38 +0000

    in continuare nu vad nicio sectiune de debug

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Cache-busting comment to force server restart and clear stale data. v6
+  // Cache-busting comment to force server restart and clear stale data. v7
   typescript: {
     ignoreBuildErrors: true,
   },
