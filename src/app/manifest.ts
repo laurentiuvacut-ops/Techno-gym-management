0eabb9ca1a90fe90b433b3861e345133caca15b1
Laurentiu Vacut <laurentiuvacut@gmail.com>
2026-02-23 22:05:25 +0000

    Cat de greu ar fi sa publicam aceasta aplicatie pe app store si magazin

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -4,21 +4,25 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Techno Gym',
     short_name: 'Techno Gym',
-    description: 'Your futuristic gym companion.',
+    description: 'Partenerul tău futurist pentru fitness și antrenamente non-stop.',
     start_url: '/dashboard',
     display: 'standalone',
     background_color: '#09090b',
     theme_color: '#09090b',
+    orientation: 'portrait',
+    categories: ['fitness', 'lifestyle', 'health'],
     icons: [
       {
         src: 'https://i.imgur.com/9W1ye1w.png',
         sizes: '192x192',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'maskable'
       },
       {
         src: 'https://i.imgur.com/9W1ye1w.png',
         sizes: '512x512',
-        type: 'image/png'
+        type: 'image/png',
+        purpose: 'any'
       }
     ]
   }
